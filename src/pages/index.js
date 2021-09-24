4e3bb610e68d103e38152972865e2f7fbcd5a1bd
Taylor Dean <taylordean787@gmail.com>
2021-09-24 12:25:29 -0500

    styling components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,18 @@
+import { Link } from "gatsby"
 import React from "react"
 import Layout from "../components/Layout"
-import Navbar from "../components/Navbar"
+import * as styles from "../styles/home.module.css"
 
 export default function Home() {
   return (
     <Layout>
-      <section>
+      <section className={styles.header}>
         <div>
           <h2>Design</h2>
           <h3>Develop and Deploy</h3>
+          <Link className={styles.btn} to="/projects">
+            My Portfolio Projects
+          </Link>
         </div>
       </section>
     </Layout>
--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,11 @@
 import React from "react"
 import Layout from "../../components/Layout"
+import * as styles from "../../styles/projects.module.css"
 
 export default function Projects() {
   return (
     <Layout>
-      <div>
+      <div className={styles.portfolio}>
         <h1>Projects Portfolio</h1>
         <p>Lorem impsum</p>
       </div>
